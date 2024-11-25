2e73bc7c407f4109e75a418a5e9bf88d94f1f441
rp0412204@gmail.com <rp0412204@gmail.com>
2024-11-25 23:46:59 +0530

    Added challenge response to verify its pdv client at port 234

--- pdv_host.py
+++ pdv_host.py
@@ -1,5 +1,9 @@
 import socket
 import netifaces
+import icmplib
+import argparse
+
+PDV_CLIENT_PORT = 234
 
 def try_get_ip_addresses(interface):
 	addrs = []
@@ -11,23 +15,54 @@ def try_get_ip_addresses(interface):
 				addrs.append(ip_addresses['addr'])
 	return addrs
 
+def check_for_pdv_client(ip_address):
+	print('checking for pdv client at port %d' % PDV_CLIENT_PORT, end = ' ')
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	except:
+		print('- socket error occurred', end = ' ')
+		return
+	s.settimeout(1)
+	try:
+		s.connect((ip_address, PDV_CLIENT_PORT))
+	except:
+		print(' - connection error occurred', end = ' ')
+		return
+	challenge = "From PDV Host: Who are you?"
+	try:
+		s.sendall(challenge.encode())
+	except:
+		s.close()
+		print(' - send error occurred', end = ' ')
+		return
+	try:
+		buf = s.recv(1024)
+	except:
+		s.close()
+		print(' - recv error ocurred', end = ' ')
+		return
+	response = buf.decode("utf-8")
+	if response == "I'm PDV Client":
+		print('- found pdv client')
+	else:
+		print('- response is wrong')
+
 def ping(ip_address):
-	print('\tPinging %s ..' % ip_address)
-	return True
+	print('\tPinging %s ..' % ip_address, end = ' ')
+	result = icmplib.ping(ip_address, count=1, interval=0.1)
+	if result.is_alive:
+		print(' - found alive', end = ' ')
+		check_for_pdv_client(ip_address)
+	print('')
 
 def find_hosts_on_subnet(ip_address):
-	hosts = [ip_address]
 	packed_addr = socket.inet_aton(ip_address)
 	for i in range(0, 256):
 		packed_ip_addr = bytes([packed_addr[0], packed_addr[1], packed_addr[2], i])
 		unpacked_ip_addr = socket.inet_ntoa(packed_ip_addr)
-		result = ping(unpacked_ip_addr)
-		if result:
-			hosts.append(unpacked_ip_addr)
-	return hosts
+		ping(unpacked_ip_addr)
 
 def find_hosts():
-	hosts = []
 	for interface in netifaces.interfaces():
 		print('Trying to get INET addresses for interface: %s' % interface)
 		ip_addresses = try_get_ip_addresses(interface)
@@ -35,17 +70,15 @@ def find_hosts():
 			print('\tIP Address: %s' % ip_address)
 			if not ip_address == '127.0.0.1':
 				print('\tFinding hosts on the same subnet as in %s' % ip_address)
-				network_hosts = find_hosts_on_subnet(ip_address)
-				hosts.extend(network_hosts)
-	return hosts
+				find_hosts_on_subnet(ip_address)
 
 def main():
 	print('PDV Host version 1.0')
-	ipa_list = find_hosts()
-#	for ipa in ipa_list:
-#		print('IP Address: %s' % ipa)
-	if len(ipa_list) == 0:
-		print('No hosts found')
+	parser = argparse.ArgumentParser(description = 'PDV Host version 1.0')
+	parser.add_argument('--port', action = "store", dest = "pdv_port", type=int, required=False)
+	given_args = parser.parse_args()
+	PDV_CLIENT_PORT = given_args.pdv_port
+	find_hosts()
 	return
 
 main()
