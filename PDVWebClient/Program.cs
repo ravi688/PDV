6f5736a23eb3cee00d87064644abd563bb627bd9
rp0412204@gmail.com <rp0412204@gmail.com>
2024-11-27 19:05:00 +0530

    Add StateContainer and listings of CPU Results in Result.razor

--- PDVWebClient/Program.cs
+++ PDVWebClient/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddScoped<StateContainer>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
