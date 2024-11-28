f0afb21b65df24a920b960dae8bed13e4bdf1772
ravi688 <rp0412204@gmail.com>
2024-11-28 19:25:10 +0530

    Added PDV_HOST_PSWD variable in db_setup.sh to specify password for pdv host user in mysql database

--- db_setup.sh
+++ db_setup.sh
@@ -16,6 +16,11 @@ if [ -z $PDV_HOST_IPA ]; then
 	PDV_HOST_IPA="127.0.0.1"
 fi
 
+if [ -z $PDV_HOST_PSWD ]; then
+	echo "Error: Variable PDV_HOST_PSWD is unset, please set it"
+	return
+fi
+
 # create database and tables
 echo "CREATE DATABASE IF NOT EXISTS ${DB_NAME};" | mysql
 echo "USE ${DB_NAME};" | mysql
@@ -24,5 +29,5 @@ echo "CREATE TABLE IF NOT EXISTS ${DB_NAME}.main_table (id INT AUTO_INCREMENT PR
 echo "CREATE USER IF NOT EXISTS '${DB_USERNAME}'@'${WC_IPA}' IDENTIFIED BY '${DB_PASSWORD}';" | mysql
 echo "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USERNAME}'@'${WC_IPA}';" | mysql
 # create user for the pdv_host.py, currenly only one pdv host is supported
-echo "CREATE USER IF NOT EXISTS 'pdvhost'@'${PDV_HOST_IPA}' IDENTIFIED BY 'Welcome@123';" | mysql
+echo "CREATE USER IF NOT EXISTS 'pdvhost'@'${PDV_HOST_IPA}' IDENTIFIED BY '${PDV_HOST_PSWD}';" | mysql
 echo "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO 'pdvhost'@'${PDV_HOST_IPA}';" | mysql
